ef64df2f05b3f2b6a27cf7c1250f783014fcabff
rcavakian <85318873+rcavakian@users.noreply.github.com>
2024-05-09 21:18:52 -0300

    Alterar arquivo app.js e adicionar arquivo .env

--- src/app.js
+++ src/app.js
@@ -1,1 +1,8 @@
-console.log("Hello world!");
+const express = require('express');
+
+const app = express();
+app.use(express.json());
+
+app.listen(3000, console.log("Servidor escutando na porta 3000"));
+
+module.exports = app;
